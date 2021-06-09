bf6be2c7246a17a09733d22a71df032800912efe
unknown <farhanrizkyyyy@gmail.com>
2021-06-09 21:43:26 +0700

    Praktikum PBO D

---
+++ ArrayExample.java
@@ -0,0 +1,23 @@
+package ArrayTest;
+
+public class ArrayExample {
+	public static void main(String[] args) {
+		String dataProdi[] = {"Administrasi", "Agroteknologi", "Teknik Elektro", 
+				"Bahasa Arab", "Bahasa Inggris", "Teknik Informatika", "Fisika", 
+				"Kimia", "Biologi", "Manajemen"};
+		
+		System.out.println("Index ke-1	: " + dataProdi[1]);
+		System.out.println("Index ke-3	: " + dataProdi[3]);
+		System.out.println("Index ke-5	: " + dataProdi[5]);
+		
+		System.out.println();
+		
+		for(int i = 0; i < dataProdi.length; i++) {
+			if(i == 3) {
+				continue;
+			}
+			
+			System.out.println("Index ke-" + i + "	: " +dataProdi[i]);
+		}
+	}
+}
